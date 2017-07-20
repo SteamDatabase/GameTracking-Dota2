a140a092a1bec2c14cf8250279b8d31032b4d28c
Wendy <freeman@steamdb.info>
2017-07-20 19:19:30 +0000

    2325 | 15 files | M _dump/lua/values0.json,  M game/bin/built_from_cl.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/pak01_dir.txt,  M game/dota/pak01_dir/panorama/layout/ability_details.xml,  M game/dota/pak01_dir/panorama/layout/popups/popup_broadcaster_popup_fact.xml,  M game/dota/pak01_dir/panorama/scripts/hud/dota_hud_spectate_heroselection.js,  M game/dota/pak01_dir/panorama/styles/ability_details.css,  M game/dota/pak01_dir/panorama/styles/popups/popup_broadcaster_popup_fact.css,  M game/dota/pak01_dir/panorama/styles/tooltips/tooltip_ability.css,  M game/dota/pak01_dir/scripts/npc/items.txt,  M game/dota/panorama/localization/dota_english.txt,  M game/dota/resource/broadcastfacts_english.txt,  M game/dota/resource/dota_english.txt,  M game/dota/steam.inf
    https://steamdb.info/patchnotes/1975780/

--- game/dota/pak01_dir/panorama/scripts/hud/dota_hud_spectate_heroselection.js
+++ game/dota/pak01_dir/panorama/scripts/hud/dota_hud_spectate_heroselection.js
@@ -8,11 +8,28 @@ function OnHeroPickDelay()
 /* Called from C++ Code */
 function OnHeroPicked( teamIndex, playerIndex )
 {
-	$.Msg( "OnHeroPicked" );
+	$.Msg( "OnHeroPicked " + teamIndex.toString() + " " + playerIndex.toString() );
 	$.Schedule( 2.0, function () {
 		OnHeroPickDelay();
 	} );
 }
+
+/* Called from C++ Code */
+function OnHeroPickStart( teamIndex, playerIndex )
+{
+	$.Msg( "OnHeroPickStart " + teamIndex.toString() + " " + playerIndex.toString() );
+}
+
+function OnHeroBanStart( teamIndex, playerIndex )
+{
+	$.Msg( "OnHeroBanStart " + teamIndex.toString() + " " + playerIndex.toString() );
+}
+
+function OnHeroBanned( teamIndex, playerIndex )
+{
+	$.Msg( "OnHeroBanned " + teamIndex.toString() + " " + playerIndex.toString() );
+}
+
 
 
 /* Called from C++ Code */
