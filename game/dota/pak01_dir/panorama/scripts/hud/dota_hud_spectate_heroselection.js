580abd2743351c284f323b767a2012e90ca5a35b
Wendy <freeman@steamdb.info>
2017-07-29 01:00:16 +0000

    2345 | 54 files | M Protobufs/dota_gcmessages_client.proto,  M Protobufs/dota_gcmessages_client_match_management.proto,  M Protobufs/dota_gcmessages_common_match_management.proto,  M Protobufs/dota_gcmessages_msgid.proto,  M _dump/lua/out1.json,  M _dump/lua/values0.json,  M _dump/lua/values1.json,  M game/bin/built_from_cl.txt,  M game/bin/linuxsteamrt64/libengine2_strings.txt,  M game/dota/bin/linuxsteamrt64/libclient.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/bin/linuxsteamrt64/libserver.txt,  M game/dota/bin/linuxsteamrt64/libserver_strings.txt,  M game/dota/maps/backgrounds/spectate_background_default.txt,  M game/dota/pak01_dir.txt,  M game/dota/pak01_dir/panorama/layout/banners/front_page_international_2017.xml,  M game/dota/pak01_dir/panorama/layout/dota_hud_spectate_hero_selection.xml,  M game/dota/pak01_dir/panorama/layout/hud/dota_hud_hero_pick_timeline.xml,  M game/dota/pak01_dir/panorama/layout/lobby.xml,  M game/dota/pak01_dir/panorama/layout/player_card.xml,  M game/dota/pak01_dir/p
    https://steamdb.info/patchnotes/1996019/

--- game/dota/pak01_dir/panorama/scripts/hud/dota_hud_spectate_heroselection.js
+++ game/dota/pak01_dir/panorama/scripts/hud/dota_hud_spectate_heroselection.js
@@ -1,23 +1,113 @@
-/* Called from JS delay */
-function OnHeroPickDelay()
-{
-	$.Msg( "OnHeroPickDelay" );
-}
-
-
 /* Called from C++ Code */
 function OnHeroPicked( teamIndex, playerIndex )
 {
 	$.Msg( "OnHeroPicked " + teamIndex.toString() + " " + playerIndex.toString() );
-	$.Schedule( 2.0, function () {
-		OnHeroPickDelay();
-	} );
+
+	if ( teamIndex == 0 )
+	{	
+		$( '#Row0' ).RemoveClass( 'ViewAll' );
+		$.DispatchEvent( 'DOTAGlobalSceneSetCameraEntity', 'RadiantScene', teamIndex + '_camera_hero' + playerIndex, 1.5 );
+		$( '#RadiantScene' ).FireEntityInput( 'fx_spot_0_'+ playerIndex, 'Stop', '' );
+		$.DispatchEvent( 'AddStyle', $('#SpectateHeroSelection'), 'RadiantFocusActive');
+		$( '#RadiantScene' ).FireEntityInput( 'fx_tease_' + teamIndex + '_' + playerIndex, 'Stop', '' );
+		$( '#RadiantScene' ).FireEntityInput( 'fx_tease_'+ teamIndex +'_'+ playerIndex, 'Start', '' );	
+	}
+	else if ( teamIndex == 1 )
+	{
+		$( '#Row1' ).RemoveClass( 'ViewAll' );
+		$.DispatchEvent( 'DOTAGlobalSceneSetCameraEntity', 'DireScene', teamIndex + '_camera_hero' + playerIndex, 1.5 );
+		$( '#DireScene' ).FireEntityInput( 'fx_spot_1_'+ playerIndex, 'Stop', '' );
+		$.DispatchEvent( 'AddStyle', $('#SpectateHeroSelection'), 'DireFocusActive');	
+		$( '#DireScene' ).FireEntityInput( 'fx_tease_' + teamIndex + '_' + playerIndex, 'Stop', '' );
+		$( '#DireScene' ).FireEntityInput( 'fx_tease_'+ teamIndex +'_'+ playerIndex, 'Start', '' );					
+	}
+
+	$.DispatchEvent( 'PlaySoundEffect', 'InspectorCam.Activate' );
 }
 
 /* Called from C++ Code */
 function OnHeroPickStart( teamIndex, playerIndex )
 {
 	$.Msg( "OnHeroPickStart " + teamIndex.toString() + " " + playerIndex.toString() );
+		
+	if ( teamIndex == 0 )
+	{	
+		var scenePanelRadiant = $( '#RadiantScene' );
+		scenePanelRadiant.FireEntityInput( 'fx_spot_' + teamIndex + '_' + playerIndex, 'Stop', '' );
+		scenePanelRadiant.FireEntityInput( 'fx_spot_'+ teamIndex +'_'+ playerIndex, 'Start', '' );		
+	}
+	else if ( teamIndex == 1 )
+	{
+		var scenePanelDire = $( '#DireScene' );
+		scenePanelDire.FireEntityInput( 'fx_spot_' + teamIndex + '_' + playerIndex, 'Stop', '' );
+		scenePanelDire.FireEntityInput( 'fx_spot_'+ teamIndex +'_'+ playerIndex, 'Start', '' );		
+	}
+
+	for (i = 0; i < 5; i++) 
+	{ 
+		$( '#BansBackground' ).RemoveClass( 'team1slot' + i );
+	}						
+	for (i = 0; i < 5; i++) 
+	{ 
+		$( '#BansBackground' ).RemoveClass( 'team0slot' + i );
+	}					
+	
+	$.DispatchEvent( 'AddStyle', $( '#BansBackground' ), 'team' + teamIndex + 'slot' + playerIndex );
+}
+
+/* Called from C++ Code */
+function OnHeroSpawned( teamIndex, playerIndex )
+{
+	$.Msg( "OnHeroSpawned " + teamIndex.toString() + " " + playerIndex.toString() );
+
+	if ( teamIndex == 0 )
+	{
+		var scenePanelRadiant = $( '#RadiantScene' );
+		scenePanelRadiant.FireEntityInput( 'fx_' + teamIndex + '_' + playerIndex, 'Stop', '' );
+		scenePanelRadiant.FireEntityInput( 'fx_' + teamIndex + '_' + playerIndex, 'Start', '' );
+	}
+	else if ( teamIndex == 1 )
+	{
+		var scenePanelDire = $( '#DireScene' );
+		scenePanelDire.FireEntityInput( 'fx_' + teamIndex + '_' + playerIndex, 'Stop', '' );
+		scenePanelDire.FireEntityInput( 'fx_' + teamIndex + '_' + playerIndex, 'Start', '' );
+	}
+}
+
+/* Called from C++ Code */
+function OnHeroSpawnedUI( teamIndex, playerIndex )
+{
+	$.Msg( "OnHeroSpawnedUI " + teamIndex.toString() + " " + playerIndex.toString() );
+
+	if ( teamIndex == 0 )
+	{
+		$( '#HeroPickedRadiant' ).AddClass( 'ShowPickedLabel' );
+	}
+	else if ( teamIndex == 1 )
+	{
+		$( '#HeroPickedDire' ).AddClass( 'ShowPickedLabel' );
+	}
+}
+
+/* Called from C++ Code */
+function OnHeroSpawnComplete(teamIndex, playerIndex)
+{
+	$.Msg("OnHeroSpawnComplete " + teamIndex.toString() + " " + playerIndex.toString());
+
+	if (teamIndex == 0)
+	{
+		$.DispatchEvent('DOTAGlobalSceneSetCameraEntity', 'RadiantScene', 'radiant_camera', 1.5);
+		$('#Row0').AddClass('ViewAll');
+		$('#SpectateHeroSelection').RemoveClass('RadiantFocusActive');
+		$('#HeroPickedRadiant').RemoveClass('ShowPickedLabel');
+	}
+	else if (teamIndex == 1)
+	{
+		$.DispatchEvent('DOTAGlobalSceneSetCameraEntity', 'DireScene', 'dire_camera', 1.5);
+		$('#Row1').AddClass('ViewAll');
+		$('#SpectateHeroSelection').RemoveClass('DireFocusActive');
+		$('#HeroPickedDire').RemoveClass('ShowPickedLabel');
+	}
 }
 
 function OnHeroBanStart( teamIndex, playerIndex )
