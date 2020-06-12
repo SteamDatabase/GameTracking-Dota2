a6f18122d3a74e7022a134fee0535694afdd1888
Wendy <freeman@steamdb.info>
2020-06-12 03:33:11 +0000

    4261 | 110 files | M game/bin/built_from_cl.txt,  M game/bin/linuxsteamrt64/libpanorama_strings.txt,  M game/bin/linuxsteamrt64/libresourcesystem_strings.txt,  M game/bin/linuxsteamrt64/libsteamnetworkingsockets.txt,  M game/bin/linuxsteamrt64/libsteamnetworkingsockets_strings.txt,  M game/bin/linuxsteamrt64/libtier0_strings.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/bin/linuxsteamrt64/libserver_strings.txt,  M game/dota/maps/backgrounds/hero_showcase_qop_arcana.txt,  M game/dota/maps/dota_ti10.txt,  M game/dota/pak01_dir.txt,  M game/dota/pak01_dir/panorama/layout/banners/front_page_battlepass_ti10_treasure_vote.xml,  M game/dota/pak01_dir/panorama/layout/battle_pass/international2020/bp_international2020_home.xml,  M game/dota/pak01_dir/panorama/layout/battle_pass/international2020/bp_international2020_reward_list.xml,  M game/dota/pak01_dir/panorama/layout/battle_pass/international2020/bp_international2020_rewards.xml,  M game/dota/pak01_dir/panorama/layout/frontpage_contents.xml,  M game/dota/p
    https://steamdb.info/patchnotes/5153670/

---
+++ game/dota/pak01_dir/panorama/scripts/leaf_pages/dashboard_page_debut_queen_of_pain_arcana.js
@@ -0,0 +1,68 @@
+
+var g_Toggle = false;
+var CameraToggle = function () {
+    var strFunction = g_Toggle ? "qop_arc_debut_camera_style_1" : "qop_arc_debut_camera_style_2";
+    g_Toggle = !g_Toggle;
+    $('#ModelBackground').FireEntityInput( 'camera_driver', 'SetAnimation', strFunction );
+}
+
+var RunPageAnimation = function ()
+{
+	var seq = new RunSequentialActions();
+
+	$( '#ModelContainer' ).RemoveAndDeleteChildren();
+	$( '#ModelContainer' ).BLoadLayoutSnippet( 'ModelSnippet' );
+
+	$( '#MainContainer' ).RemoveClass( 'Initialize' );
+	$( '#ModelBackground' ).RemoveClass( 'Initialize' );
+	$( '#DebutInformation' ).RemoveClass( 'Initialize' );
+	$( '#InformationBody' ).RemoveClass( 'Initialize' );
+	$( '#InformationBodyBackground' ).RemoveClass( 'Initialize' );
+	$( '#ItemLore' ).RemoveClass( 'Initialize' );
+    $('#AlternateStyleButton').RemoveClass('Initialize');
+    $('#DefaultStyleButton').RemoveClass('Initialize');
+
+	seq.actions.push( new WaitAction( 0.01 ) );
+    seq.actions.push( new RunFunctionAction( function() { $.DispatchEvent( 'DOTASetCurrentDashboardPageFullscreen', true ); } ) )
+	seq.actions.push( new WaitForClassAction( $( '#ModelBackground' ), 'SceneLoaded' ) );
+    seq.actions.push( new RunFunctionAction( function () { $('#ModelBackground').FireEntityInput('qop_arcana_alt', "Disable", '0'); } ) )
+	seq.actions.push( new RunFunctionAction( function() { $.DispatchEvent( 'PlaySoundEffect', 'qop_arcana_takeover_stinger' ); } ) )
+	seq.actions.push( new WaitAction( 0.25 ) );
+	seq.actions.push( new AddClassAction( $( '#MainContainer' ), 'Initialize' ) );
+	seq.actions.push( new WaitAction( 0.0 ) );
+	seq.actions.push( new AddClassAction( $( '#ModelBackground' ), 'Initialize' ) );
+	seq.actions.push( new WaitAction( 2.0 ) );
+	seq.actions.push( new AddClassAction( $( '#DebutInformation' ), 'Initialize' ) );
+	seq.actions.push( new AddClassAction( $( '#InformationBody' ), 'Initialize' ) );
+	seq.actions.push( new AddClassAction( $( '#InformationBodyBackground' ), 'Initialize' ) );
+	seq.actions.push( new AddClassAction( $( '#ItemLore' ), 'Initialize' ) );
+    seq.actions.push( new AddClassAction( $( '#AlternateStyleButton'), 'Initialize' ) );
+    seq.actions.push( new AddClassAction( $( '#DefaultStyleButton'), 'Initialize' ) );
+
+	seq.actions.push( new RunFunctionAction( function() 
+	{
+	    $('#ModelBackground').SetRotateParams( -1, 1, -1, 1 );
+	}));
+
+	RunSingleAction( seq );
+}
+
+function alternateStyle() {
+    $.GetContextPanel().AddClass('ShowingAlternateStyle');
+    CameraToggle();
+    $( '#ModelBackground' ).FireEntityInput( 'qop_arcana_alt', 'SetActivityModifier', 'debut_alt_style' );
+    $.Schedule(0.4, function () {
+        $('#ModelBackground').FireEntityInput( 'qop_arcana_alt', "Enable", '0' );
+        $('#ModelBackground').FireEntityInput( 'qop_arcana_alt', 'SetActivity', 'ACT_DOTA_SPAWN' );
+    } );
+    $.Schedule(1.0, function () { $( '#ModelBackground' ).FireEntityInput( 'qop_arcana_alt', 'SetActivity', 'ACT_DOTA_LOADOUT' ); } );
+}
+
+function originalStyle() {
+    $.GetContextPanel().RemoveClass( 'ShowingAlternateStyle' );
+    // play blue blink fx
+    CameraToggle();
+    $( '#ModelBackground' ).FireEntityInput( 'qop_arcana_alt', 'SetActivityModifier', 'debut_alt_style' );
+    $( '#ModelBackground' ).FireEntityInput( 'qop_arcana_alt', 'SetActivity', 'ACT_BARNACLE_CHEW' );
+    $.Schedule(0.5, function () { $('#ModelBackground').FireEntityInput('qop_arcana_alt', "Disable", '0'); } );
+}
