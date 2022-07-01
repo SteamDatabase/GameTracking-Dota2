a89a7e6534b8462411f9782949c108e0c26fa4c0
Wendy <freeman@steamdb.info>
2022-07-01 02:59:30 +0000

    5354 | 3 files | M game/bin/built_from_cl.txt,  M game/dota/steam.inf,  M game/dota_addons/conquest/scripts/vscripts/boulder_trap_ai.lua
    https://steamdb.info/patchnotes/9041501/

--- game/dota_addons/conquest/scripts/vscripts/boulder_trap_ai.lua
+++ game/dota_addons/conquest/scripts/vscripts/boulder_trap_ai.lua
@@ -37,27 +37,35 @@ function Fire(trigger)
 		return
 	end
 
-	if heroHandle ~= nil then
-		if heroHandle:IsOutOfGame() == true or heroHandle:IsBuilding() == true or trigger.activator:GetName() == thisEntity:GetName() or heroHandle.isDigSite == true then
-			--print(trigger.activator:GetName().." cannot be hit by boulder")
-			return
-		end
+	--print(" Boulder hit hero "..trigger.activator:GetName().." triggered by "..thisEntity.KillerToCredit:GetName().." at "..GameRules:GetDOTATime(false, false))
 
-		--print("Boulder should kill a hero")
-		local boulderTrap = thisEntity:FindAbilityByName("boulder_trap")
-		--thisEntity:CastAbilityOnTarget(heroHandle, boulderTrap, -1 )
-		-- Using Kill() instead of ability
-		local killEffects = ParticleManager:CreateParticle( "particles/units/heroes/hero_life_stealer/life_stealer_infest_emerge_blood01.vpcf", PATTACH_POINT, heroHandle )
-		ParticleManager:SetParticleControlEnt( killEffects, 0, heroHandle, PATTACH_POINT, "attach_hitloc", heroHandle:GetAbsOrigin(), false )
-		heroHandle:Attribute_SetIntValue( "effectsID", killEffects )
-		EmitSoundOn( "Conquest.Pendulum.Target", heroHandle )
-		local killerEntity = thisEntity
-		if thisEntity.KillerToCredit ~= nil then
-			killerEntity = thisEntity.KillerToCredit
-		end
-		heroHandle:Kill( nil, killerEntity )
-	else
-		print("Boulder fail: HeroHandle is null")
+	if heroHandle == nil or heroHandle:IsNull() then
+		return
 	end
+
+	local nResult = UnitFilter( heroHandle, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_OTHER, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_TEAM_GOODGUYS )
+	--print( "$$$$Result: " .. nResult )
+	if nResult ~= UF_SUCCESS then
+		return
+	end
+
+	if heroHandle:IsPhantom() or trigger.activator:GetName() == thisEntity:GetName() or heroHandle.isDigSite == true then
+		--print(trigger.activator:GetName().." cannot be hit by boulder")
+		return
+	end
+
+	--print("Boulder should kill a hero")
+	local boulderTrap = thisEntity:FindAbilityByName("boulder_trap")
+	--thisEntity:CastAbilityOnTarget(heroHandle, boulderTrap, -1 )
+	-- Using Kill() instead of ability
+	local killEffects = ParticleManager:CreateParticle( "particles/units/heroes/hero_life_stealer/life_stealer_infest_emerge_blood01.vpcf", PATTACH_POINT, heroHandle )
+	ParticleManager:SetParticleControlEnt( killEffects, 0, heroHandle, PATTACH_POINT, "attach_hitloc", heroHandle:GetAbsOrigin(), false )
+	heroHandle:Attribute_SetIntValue( "effectsID", killEffects )
+	EmitSoundOn( "Conquest.Pendulum.Target", heroHandle )
+	local killerEntity = thisEntity
+	if thisEntity.KillerToCredit ~= nil then
+		killerEntity = thisEntity.KillerToCredit
+	end
+	heroHandle:Kill( nil, killerEntity )
 end
 
