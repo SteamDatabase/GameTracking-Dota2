9cb35b9141b9b10b22ad9ca5cec7a9c35370cdcc
Wendy <freeman@steamdb.info>
2023-01-07 00:32:04 +0000

    5588 | 44 files | M Protobufs/dota_gcmessages_server.proto,  M game/bin/built_from_cl.txt,  M game/dota/bin/linuxsteamrt64/libserver_strings.txt,  M game/dota/pak01_dir.txt,  M game/dota/pak01_dir/resource/localization/abilities_finnish.txt,  M game/dota/pak01_dir/resource/localization/abilities_french.txt,  M game/dota/pak01_dir/resource/localization/abilities_german.txt,  M game/dota/pak01_dir/resource/localization/dota_brazilian.txt,  M game/dota/pak01_dir/resource/localization/dota_bulgarian.txt,  M game/dota/pak01_dir/resource/localization/dota_czech.txt,  M game/dota/pak01_dir/resource/localization/dota_danish.txt,  M game/dota/pak01_dir/resource/localization/dota_french.txt,  M game/dota/pak01_dir/resource/localization/dota_german.txt,  M game/dota/pak01_dir/resource/localization/dota_greek.txt,  M game/dota/pak01_dir/resource/localization/dota_hungarian.txt,  M game/dota/pak01_dir/resource/localization/dota_italian.txt,  M game/dota/pak01_dir/resource/localization/dota_koreana.txt,  M game/dota/pak01_dir/resource/local
    https://steamdb.info/patchnotes/10272074/

--- game/dota_addons/winter2022/scripts/vscripts/modifiers/gameplay/modifier_building_roshan_attacking.lua
+++ game/dota_addons/winter2022/scripts/vscripts/modifiers/gameplay/modifier_building_roshan_attacking.lua
@@ -13,4 +13,25 @@ function modifier_building_roshan_attacking:IsPurgable()
 	return false
 end
 
+--------------------------------------------------------------------------------
+
+function modifier_building_roshan_attacking:GetPriority()
+	return MODIFIER_PRIORITY_ULTRA + 10000
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_building_roshan_attacking:CheckState()
+	if IsServer() == false then
+		return
+	end
+
+	local state =
+	{
+		[MODIFIER_STATE_INVULNERABLE] = false,
+	}
+
+	return state
+end
+
 -------------------------------------------------------------------------------
