c42f95a492a133b2ebb2ebebe98ac7847fc67743
Wendy <freeman@steamdb.info>
2024-08-13 20:31:47 +0000

    sort strings

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ echo "Processing Dota 2..."
 
 ProcessDepot ".dll"
 ProcessDepot ".so"
+DeduplicateStringsFrom ".so" "game/bin/linuxsteamrt64/libengine2_strings.txt" "game/bin/linuxsteamrt64/libtier0_strings.txt"
 ProcessVPK
 ProcessToolAssetInfo
 FixUCS2
