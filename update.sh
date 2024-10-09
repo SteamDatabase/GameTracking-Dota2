b94169f505209f3ecd7fd21bed3361686bd4b894
Wendy <freeman@steamdb.info>
2024-10-09 07:40:40 +0000

    Deduplicate schema dumps from strings

--- update.sh
+++ update.sh
@@ -5,13 +5,13 @@ cd "${0%/*}"
 
 echo "Processing Dota 2..."
 
+../tools/dump_source2.sh dota dota
+
 ProcessDepot ".dll"
 ProcessDepot ".so"
-DeduplicateStringsFrom ".so" "game/bin/linuxsteamrt64/libengine2_strings.txt" "game/bin/linuxsteamrt64/libtier0_strings.txt"
+DeduplicateStringsFrom ".so" "game/bin/linuxsteamrt64/libengine2_strings.txt" "game/bin/linuxsteamrt64/libtier0_strings.txt" "DumpSource2/.stringsignore"
 ProcessVPK
 ProcessToolAssetInfo
-
-../tools/dump_source2.sh dota dota
 
 FixUCS2
 
