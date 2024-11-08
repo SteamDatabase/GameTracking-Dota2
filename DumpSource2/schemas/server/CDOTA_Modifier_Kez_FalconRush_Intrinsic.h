ab2ab14f856d41dee0c515fe25b38c4615589028
Wendy <freeman@steamdb.info>
2024-11-08 23:28:44 +0000

    6242 | 7 files | M DumpSource2/schemas/client/CDOTA_Modifier_Kez_FalconRush_Intrinsic.h,  M DumpSource2/schemas/server/CDOTA_Modifier_Kez_FalconRush_Intrinsic.h,  M game/bin/built_from_cl.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/bin/linuxsteamrt64/libserver_strings.txt,  M game/dota/pak01_dir.txt,  M game/dota/steam.inf
    https://steamdb.info/patchnotes/16348103/

--- DumpSource2/schemas/server/CDOTA_Modifier_Kez_FalconRush_Intrinsic.h
+++ DumpSource2/schemas/server/CDOTA_Modifier_Kez_FalconRush_Intrinsic.h
@@ -1,6 +1,7 @@
 class CDOTA_Modifier_Kez_FalconRush_Intrinsic : public CDOTA_Buff
 {
-	float32 base_attack_rate;
+	float32 base_attack_rate_katana;
+	float32 base_attack_rate_sai;
 	int32 attack_speed_factor;
 	float32 m_flAttackRate;
 };
