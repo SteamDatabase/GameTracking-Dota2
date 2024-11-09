535597bcc793f97a263f008c50fd420df1fdc05b
Wendy <freeman@steamdb.info>
2024-11-09 04:57:34 +0000

    6243 | 6 files | M DumpSource2/schemas/client/CSurvivorsSpawnerDestructibles.h,  M DumpSource2/schemas/client/CSurvivorsSpawnerDestructiblesDefinition.h,  M game/bin/built_from_cl.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/pak01_dir.txt,  M game/dota/steam.inf
    https://steamdb.info/patchnotes/16349778/

--- DumpSource2/schemas/client/CSurvivorsSpawnerDestructibles.h
+++ DumpSource2/schemas/client/CSurvivorsSpawnerDestructibles.h
@@ -1,5 +1,5 @@
 class CSurvivorsSpawnerDestructibles : public CSurvivorsSpawner
 {
-	float32 m_flMinimiumDistanceBetween;
+	float32 m_flMinimumDistanceBetween;
 	float32 m_flDestroyDistance;
 };
--- DumpSource2/schemas/client/CSurvivorsSpawnerDestructiblesDefinition.h
+++ DumpSource2/schemas/client/CSurvivorsSpawnerDestructiblesDefinition.h
@@ -1,5 +1,5 @@
 class CSurvivorsSpawnerDestructiblesDefinition : public CSurvivorsSpawnerDefinition
 {
-	float32 m_flMinimiumDistanceBetween;
+	float32 m_flMinimumDistanceBetween;
 	float32 m_flDestroyDistance;
 };
