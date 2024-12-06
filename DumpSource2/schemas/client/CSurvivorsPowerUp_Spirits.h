fc132c5f07a613a3c01f5ddaf5055cb2cf3ce535
Wendy <freeman@steamdb.info>
2024-12-06 00:45:55 +0000

    6273 | 7 files | M DumpSource2/schemas/client/CSurvivorsPowerUp_Spirits.h,  M Protobufs/dota_gcmessages_common_overworld.proto,  M game/bin/built_from_cl.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/pak01_dir.txt,  M game/dota/readonly_tools_asset_info.txt,  M game/dota/steam.inf
    https://steamdb.info/patchnotes/16650390/

--- DumpSource2/schemas/client/CSurvivorsPowerUp_Spirits.h
+++ DumpSource2/schemas/client/CSurvivorsPowerUp_Spirits.h
@@ -4,4 +4,5 @@ class CSurvivorsPowerUp_Spirits : public CSurvivorsPowerUp
 	CResourceNameTyped< CWeakHandle< InfoForResourceTypeIParticleSystemDefinition > > m_sExplosionParticle;
 	bool m_bActive;
 	float32 m_flDurationTimer;
+	float32 m_flRange;
 };
