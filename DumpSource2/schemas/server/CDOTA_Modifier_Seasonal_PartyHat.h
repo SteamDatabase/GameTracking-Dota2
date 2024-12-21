c426331fb7c6e4a6cf38bbdc926284bc53b2e34b
Wendy <freeman@steamdb.info>
2024-12-21 02:16:14 +0000

    6283 | 7 files | M DumpSource2/schemas/server/CDOTA_Modifier_Seasonal_PartyHat.h,  M game/bin/built_from_cl.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/bin/linuxsteamrt64/libserver_strings.txt,  M game/dota/pak01_dir.txt,  M game/dota/readonly_tools_asset_info.txt,  M game/dota/steam.inf
    https://steamdb.info/patchnotes/16821123/

--- DumpSource2/schemas/server/CDOTA_Modifier_Seasonal_PartyHat.h
+++ DumpSource2/schemas/server/CDOTA_Modifier_Seasonal_PartyHat.h
@@ -2,8 +2,7 @@ class CDOTA_Modifier_Seasonal_PartyHat : public CDOTA_Buff
 {
 	int32 controlled_unit_search_radius;
 	bool child_modifier;
-	CUtlString m_strEffectName;
-	ParticleIndex_t m_nHatFXIndex;
+	CUtlVector< ParticleIndex_t > m_vecParticles;
 	CUtlVector< int32 > m_vecHatColors;
 	uint32 m_unVersion;
 	CUtlVector< CHandle< CBaseEntity > > m_vecNearbyUnits;
