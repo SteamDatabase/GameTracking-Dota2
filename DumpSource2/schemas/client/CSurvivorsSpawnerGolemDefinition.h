369a1312af8f8dbcc195504b14b8fe0cdbcd77f3
Wendy <freeman@steamdb.info>
2025-08-29 01:15:55 +0000

    6528 | 9 files | M DumpSource2/schemas/animgraphlib/AimMatrixOpFixedSettings_t.h,  M DumpSource2/schemas/animgraphlib/CMotionGraphGroup.h,  M DumpSource2/schemas/client/CSurvivorsSpawnerDefinition.h,  M DumpSource2/schemas/client/CSurvivorsSpawnerGolemDefinition.h,  M DumpSource2/schemas/scenesystem/CSSDSMsg_ViewRender.h,  M game/bin/built_from_cl.txt,  M game/dota/pak01_dir.txt,  M game/dota/readonly_tools_asset_info.txt,  M game/dota/steam.inf
    https://steamdb.info/patchnotes/19778646/

--- DumpSource2/schemas/client/CSurvivorsSpawnerGolemDefinition.h
+++ DumpSource2/schemas/client/CSurvivorsSpawnerGolemDefinition.h
@@ -19,7 +19,7 @@
 //	],
 //	"m_sSpawnInfoTargetName": "",
 //	"m_sMinimapIconClass": "",
-//	"m_flPerpendicularWallSpacing": -0.000000,
+//	"m_flPerpendicularWallSpacing": 0.000000,
 //	"m_bIgnoreDifficultySpawnMultiplier": false,
 //	"m_eSpawnPositionsLayer": "ENEMY_MAIN"
 //}
