0158ca7ea9f9e54294debd38c97764964a4c8e8c
Wendy <freeman@steamdb.info>
2025-09-05 15:25:24 +0000

    6537 | 13 files | M DumpSource2/schemas/animgraphlib/AimMatrixOpFixedSettings_t.h,  M DumpSource2/schemas/animgraphlib/CMotionGraphGroup.h,  M DumpSource2/schemas/animgraphlib/LookAtOpFixedSettings_t.h,  M DumpSource2/schemas/client/CSurvivorsSpawnerDefinition.h,  M DumpSource2/schemas/client/CSurvivorsSpawnerGolemDefinition.h,  M DumpSource2/schemas/modellib/SkeletonAnimCapture_t__Frame_t.h,  M DumpSource2/schemas/scenesystem/CSSDSMsg_ViewRender.h,  M DumpSource2/schemas/soundsystem_voicecontainers/CVoiceContainerBlender.h,  M DumpSource2/schemas/worldrenderer/ClutterTile_t.h,  M game/bin/built_from_cl.txt,  M game/dota/bin/linuxsteamrt64/libclient_strings.txt,  M game/dota/bin/linuxsteamrt64/libserver_strings.txt,  M game/dota/steam.inf
    https://steamdb.info/patchnotes/19870852/

--- DumpSource2/schemas/scenesystem/CSSDSMsg_ViewRender.h
+++ DumpSource2/schemas/scenesystem/CSSDSMsg_ViewRender.h
@@ -2,7 +2,7 @@
 //	"m_viewId":
 //	{
 //		"m_nViewId": 0,
-//		"m_nFrameCount": 140657147605377
+//		"m_nFrameCount": 139898995700097
 //	},
 //	"m_ViewName": ""
 //}
--- DumpSource2/schemas/soundsystem_voicecontainers/CVoiceContainerBlender.h
+++ DumpSource2/schemas/soundsystem_voicecontainers/CVoiceContainerBlender.h
@@ -31,7 +31,7 @@
 //		"m_sound": "",
 //		"m_pSound": null
 //	},
-//	"m_flBlendFactor": -0.000000
+//	"m_flBlendFactor": 1.655435
 //}
 // MPropertyFriendlyName = "Blender"
 // MPropertyDescription = "Blends two containers."
